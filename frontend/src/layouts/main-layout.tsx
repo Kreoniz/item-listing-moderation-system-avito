573a0c7c9f8fcbc60487f5c82c3300086fdd93ae
Kreoniz <kreoniz@mail.ru>
2025-11-16 00:15:50 +0600

    feat(frontend): add dark theme

--- frontend/src/layouts/main-layout.tsx
+++ frontend/src/layouts/main-layout.tsx
@@ -1,22 +1,35 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { NavLink, Outlet } from "react-router";
 
 export function MainLayout() {
   return (
-    <div className="mx-auto max-w-7xl p-4">
-      <nav className="mb-4">
-        <ul className="flex gap-2">
-          <li>
-            <NavLink prefetch="intent" to="/list" className="hover:underline">
-              Главная страница
-            </NavLink>
-          </li>
+    <div className="mx-auto max-w-7xl px-2 py-4 sm:px-4">
+      <nav className="mb-4 flex flex-col items-center justify-between gap-3 sm:flex-row">
+        <div className="text-xl font-bold">Модероратор</div>
 
-          <li>
-            <NavLink prefetch="intent" to="/stats" className="hover:underline">
-              Статистика
-            </NavLink>
-          </li>
-        </ul>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+
+          <ul className="flex gap-2">
+            <li>
+              <NavLink prefetch="intent" to="/list" className="hover:underline">
+                Главная страница
+              </NavLink>
+            </li>
+
+            <li>|</li>
+
+            <li>
+              <NavLink
+                prefetch="intent"
+                to="/stats"
+                className="hover:underline"
+              >
+                Статистика
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       <Outlet />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/index.css";
 import { MainLayout } from "@/layouts/main-layout";
 import { ItemPage } from "@/pages/item-page";
@@ -15,15 +16,17 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route index element={<Navigate to="/list" replace />} />
-            <Route path="/list" element={<MainPage />} />
-            <Route path="/item/:id" element={<ItemPage />} />
-            <Route path="/stats" element={<StatsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route index element={<Navigate to="/list" replace />} />
+              <Route path="/list" element={<MainPage />} />
+              <Route path="/item/:id" element={<ItemPage />} />
+              <Route path="/stats" element={<StatsPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
